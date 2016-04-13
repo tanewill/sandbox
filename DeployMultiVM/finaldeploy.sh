3abc70dac7f18f42d99b47756fcdb1991e947e3d
Taylor Newill <tanewill@microsoft.com>
2016-04-13 05:39:30 -0400

    Update finaldeploy.sh

--- DeployMultiVM/finaldeploy.sh
+++ DeployMultiVM/finaldeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/tanewill/tanewill/sandbox/DeployMultiVM/localDeploy.sh
+wget https://raw.githubusercontent.com/tanewill/sandbox/Ubuntu/DeployMultiVM/localDeploy.sh
 chmod +x localDeploy.sh
 runuser -l azureuser -c 'localDeploy.sh'
