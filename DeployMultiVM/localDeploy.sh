96c0e080f2c79e51bde8a604076bc3cbda7f8da1
Taylor Newill <tanewill@microsoft.com>
2016-04-13 13:12:59 -0400

    Update localDeploy.sh

--- DeployMultiVM/localDeploy.sh
+++ DeployMultiVM/localDeploy.sh
@@ -25,6 +25,6 @@ runuser -l azureuser -c "bin/myClusRun.sh 'DEBIAN_FRONTEND=noninteractive sudo a
 runuser -l azureuser -c "bin/myClusRun.sh 'DEBIAN_FRONTEND=noninteractive sudo apt-get -qq --yes --force-yes install paraviewopenfoam44'"
 
 #configure OpenFOAM
-runuser -l azureuser -c 'mkdir -p $FOAM_RUN'
-runuser -l azureuser -c 'cp -r $FOAM_TUTORIALS $FOAM_RUN'
+runuser -l azureuser -c '. .bashrc | mkdir -p $FOAM_RUN'
+runuser -l azureuser -c '. .bashrc | cp -r $FOAM_TUTORIALS $FOAM_RUN'
 
