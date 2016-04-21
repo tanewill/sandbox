0406f85a536b2c8d4ba27ce6ac117d050a1c9297
Taylor Newill <tanewill@microsoft.com>
2016-04-21 10:15:16 -0400

    Update CentOS-HPC_7.1_deploy.sh

--- DeployMultiVM/CentOS-HPC_7.1_deploy.sh
+++ DeployMultiVM/CentOS-HPC_7.1_deploy.sh
@@ -4,13 +4,5 @@ yum install -y nmap sshpass
 cd /home/azureuser/
 
 
-runuser -l azureuser -c 'wget https://raw.githubusercontent.com/tanewill/utils/master/authMe.sh'
-runuser -l azureuser -c 'mkdir -p /home/azureuser/.ssh'
-runuser -l azureuser -c "echo -e  'y\n' | ssh-keygen -f .ssh/id_rsa -t rsa -N ''"
-runuser -l azureuser -c 'chmod +x /home/azureuser/authMe.sh'
-mkdir -p .ssh
-echo 'Host *' >> .ssh/config
-echo 'StrictHostKeyChecking no' >> .ssh/config
-chmod 400 .ssh/config
-chown azureuser:azureuser /home/azureuser/.ssh/config
-runuser -l azureuser -c 'bash /home/azureuser/authMe.sh'
+runuser -l azureuser -c 'wget https://raw.githubusercontent.com/tanewill/utils/master/authMe2.sh'
+runuser -l azureuser -c 'bash /home/azureuser/authMe2.sh'
