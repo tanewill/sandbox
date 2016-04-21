62ea1c4e853cba94ac4a553699b380ff69e4ca65
Taylor Newill <tanewill@microsoft.com>
2016-04-20 21:53:25 -0400

    Update CentOS-HPC_7.1_deploy.sh

--- DeployMultiVM/CentOS-HPC_7.1_deploy.sh
+++ DeployMultiVM/CentOS-HPC_7.1_deploy.sh
@@ -7,4 +7,7 @@ runuser -l azureuser -c 'wget https://raw.githubusercontent.com/tanewill/utils/m
 runuser -l azureuser -c 'mkdir -p /home/azureuser/.ssh'
 runuser -l azureuser -c "echo -e  'y\n' | ssh-keygen -f .ssh/id_rsa -t rsa -N ''"
 runuser -l azureuser -c 'chmod +x /home/azureuser/authMe.sh'
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'echo Host *' >>  /home/$USER/.ssh/config
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'echo StrictHostKeyChecking no' >> /home/$USER/.ssh/config
+  sshpass -p $PASS ssh -o ConnectTimeout=2 $USER@$NAME 'chmod 400 /home/'$USER'/.ssh/config'
 runuser -l azureuser -c 'bash /home/azureuser/authMe.sh'
