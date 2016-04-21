7544755bfb7535d1647e4cd719a329e5e1ab067d
Taylor Newill <tanewill@microsoft.com>
2016-04-20 20:32:05 -0400

    Update CentOS-HPC_7.1_deploy.sh

--- DeployMultiVM/CentOS-HPC_7.1_deploy.sh
+++ DeployMultiVM/CentOS-HPC_7.1_deploy.sh
@@ -1,9 +1,8 @@
 #place holder for 7.1 script
 yum install -y epel-release
 yum install -y nmap sshpass
-nmap -sn 10.0.0.* | grep 10.0.0. | awk '{print $5}' > nodenames.txt
-
 cd /home/azureuser/
+nmap -sn 10.0.0.* | grep 10.0.0. | awk '{print $5}' > nodenames.txt
 runuser -l azureuser -c 'wget https://raw.githubusercontent.com/tanewill/utils/master/authMe.sh'
 runuser -l azureuser -c 'mkdir -p /home/azureuser/.ssh'
 runuser -l azureuser -c "echo -e  'y\n' | ssh-keygen -f .ssh/id_rsa -t rsa -N ''"
