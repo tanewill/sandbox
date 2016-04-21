66fc0c5dd9f738768338a5d6d9455604955f0549
Taylor Newill <tanewill@microsoft.com>
2016-04-20 20:46:24 -0400

    Update CentOS-HPC_7.1_deploy.sh

--- DeployMultiVM/CentOS-HPC_7.1_deploy.sh
+++ DeployMultiVM/CentOS-HPC_7.1_deploy.sh
@@ -6,5 +6,5 @@ nmap -sn 10.0.0.* | grep 10.0.0. | awk '{print $5}' > nodenames.txt
 runuser -l azureuser -c 'wget https://raw.githubusercontent.com/tanewill/utils/master/authMe.sh'
 runuser -l azureuser -c 'mkdir -p /home/azureuser/.ssh'
 runuser -l azureuser -c "echo -e  'y\n' | ssh-keygen -f .ssh/id_rsa -t rsa -N ''"
-runuser -l azureuser -c 'chmod +x authMe.sh'
-runuser -l azureuser -c 'authMe.sh'
+runuser -l azureuser -c 'chmod +x /home/azureuser/authMe.sh'
+runuser -l azureuser -c 'bash /home/azureuser/authMe.sh'
